b03dbb2e839de5a5e190a110dc71ec45c75fa04b
Kevin Van Leer <kevin.vanleer@gmail.com>
2023-05-25 10:20:11 -0500

    Updated install script to install common files

--- install.sh
+++ install.sh
@@ -5,8 +5,19 @@ if [ "$1" == "" ] ; then
   exit
 fi
 
-BASEDIR=$(dirname $(readlink -f ${BASH_SOURCE}))/$1
+SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE}))
+BASEDIR=$SCRIPT_DIR/common
+cd $BASEDIR
+FILES=$(find . -type f)
 
+for f in $FILES ; do
+  echo $f
+  echo $BASEDIR/$f
+  echo $HOME/$f
+  ln -sf $(readlink -f $BASEDIR/$f) $HOME/$f
+done
+
+BASEDIR=$SCRIPT_DIR/$1
 cd $BASEDIR
 FILES=$(find . -type f)
 
